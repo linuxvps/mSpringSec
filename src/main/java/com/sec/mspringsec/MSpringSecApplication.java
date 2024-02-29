4d39d97d4ef9406dd7eafa701034d54d8413b215
nima <linux.vps.vpn@gmail.com>
2024-02-29 18:15:53 +0330

    first commit

---
+++ src/main/java/com/sec/mspringsec/MSpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.sec.mspringsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSpringSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSpringSecApplication.class, args);
+    }
+
+}
