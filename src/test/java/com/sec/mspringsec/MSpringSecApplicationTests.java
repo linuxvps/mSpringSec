698eac464310524d2a8c3ae7c3ea601083c6b055
nima <linux.vps.vpn@gmail.com>
2024-02-29 18:15:53 +0330

    first commit

---
+++ src/test/java/com/sec/mspringsec/MSpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sec.mspringsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSpringSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
