4e11a1ea05eb1fb4c461c1c9379806bf4488470c
nima <linux.vps.vpn@gmail.com>
2024-03-01 01:03:16 +0330

    override default Security Filter Chain

--- src/main/java/com/sec/mspringsec/config/AppConfig.java
+++ src/main/java/com/sec/mspringsec/config/AppConfig.java
@@ -1,10 +1,22 @@
 package com.sec.mspringsec.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configurable
 public class AppConfig {
 
 
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
 
 }
