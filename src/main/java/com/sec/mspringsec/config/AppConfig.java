39250d5c30e9622c83618c84504b8c3df6c1355d
nima <linux.vps.vpn@gmail.com>
2024-03-01 15:15:35 +0330

    fix some bug

--- src/main/java/com/sec/mspringsec/config/AppConfig.java
+++ src/main/java/com/sec/mspringsec/config/AppConfig.java
@@ -2,12 +2,16 @@ package com.sec.mspringsec.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configurable
+@Configuration
 public class AppConfig {
 
 
@@ -17,6 +21,25 @@ public class AppConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("nimajan")
+                .password("12345")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder().
+                username("user")
+                .password("1234")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(admin, user);
+
+
     }
 
 }
