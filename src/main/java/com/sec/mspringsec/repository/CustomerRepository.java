bb84ec953c6655ce469c58086e3010cd7c18564a
nima <linux.vps.vpn@gmail.com>
2024-03-01 16:55:33 +0330

    add custom user table

---
+++ src/main/java/com/sec/mspringsec/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.sec.mspringsec.repository;
+
+import com.sec.mspringsec.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+    List<Customer> findByEmail(String email);
+}
