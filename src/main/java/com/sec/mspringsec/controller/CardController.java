073a92f880e1a2b26401f3ef9920aef9b797583d
nima <linux.vps.vpn@gmail.com>
2024-03-01 15:15:35 +0330

    fix some bug

--- src/main/java/com/sec/mspringsec/controller/CardController.java
+++ src/main/java/com/sec/mspringsec/controller/CardController.java
@@ -3,9 +3,9 @@ package com.sec.mspringsec.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping
+@RestController
 public class CardController {
     @GetMapping("/card")
     public String getCard(){
