2eaa73b5fb0dfff127273e012c109b0a05653fe6
nima <linux.vps.vpn@gmail.com>
2024-03-01 15:33:24 +0330

    add database user

--- src/main/java/com/sec/mspringsec/config/AppConfig.java
+++ src/main/java/com/sec/mspringsec/config/AppConfig.java
@@ -3,19 +3,18 @@ package com.sec.mspringsec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class AppConfig {
-
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
@@ -24,15 +23,19 @@ public class AppConfig {
         return http.build();
     }
 
+/*
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-
         UserDetails admin = User.withUsername("nimajan").password("12345").build();
-
         UserDetails user = User.withUsername("user").password("1234").build();
-
         return new InMemoryUserDetailsManager(admin, user);
 
+    }
+*/
+
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
